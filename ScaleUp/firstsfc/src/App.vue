b21900f0a50476fad078c1e62eeb6b733cc34cbd
DEVCOMP\erymn <erymnur@gmail.com>
2026-03-17 11:14:01 +0700

    implement components

--- ScaleUp/firstsfc/src/App.vue
+++ ScaleUp/firstsfc/src/App.vue
@@ -16,6 +16,20 @@
   }
 </script>
 
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+  #app > div:hover {
+    cursor: pointer;
+  }
+</style>
+
 <!-- <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
--- ScaleUp/firstsfc/src/components/FoodItem.vue
+++ ScaleUp/firstsfc/src/components/FoodItem.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
+    <div v-on:click="countClicks">
         <h2>{{ name }}</h2>
         <p>{{ message }}</p>
+        <p id="red">Kamu telah klik aku {{ clicks }} klik</p>
     </div>
 </template>
 
@@ -10,8 +11,21 @@
         data() {
             return  {
                 name: 'Apel',
-                message: 'Aku seneng apel'
+                message: 'Aku seneng apel',
+                clicks: 0
+            }
+        },
+        methods: {
+            countClicks() {
+                this.clicks++
             }
         }
     }
 </script>
+
+<style>
+    #red {
+        font-weight: bold ;
+        color: rgb(144, 12, 12);
+    }
+</style>
