4c558dd97c22ade9a13cdc91337f53553864c754
DEVCOMP\erymn <erymnur@gmail.com>
2026-04-07 17:29:37 +0700

    Teleport

---
+++ ScaleUp/firstsfc/src/components/VueTeleport.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Component</h2>
+        <p>This is inside the component.</p>
+        <Teleport to="body">
+            <div id="redDiv">Hello! This is teleported.</div>
+        </Teleport>
+    </div>
+</template>
+
+<style scoped>
+#redDiv {
+    background-color: lightcoral;
+    margin: 10px;
+    padding: 10px;
+    display: inline-block;
+}
+</style>
