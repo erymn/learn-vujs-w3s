7b465a9a4f0a3af19f878e3b1809062ee1fccf6b
DEVCOMP\erymn <erymnur@gmail.com>
2026-04-07 16:17:51 +0700

    Keep Alive
    With <KeepAlive :max="n">, the browser will only remember the user input of the last n visited components

--- ScaleUp/firstsfc/src/App6.vue
+++ ScaleUp/firstsfc/src/App6.vue
@@ -4,7 +4,8 @@
     <button @click="toggleValue = !toggleValue">Switch component</button>
     <!--Digunakan untuk simpan state ketika terjadi perpindahan komponen-->
     <!-- <KeepAlive exclude="CompTwo" include="CompOne"> -->
-    <KeepAlive include="CompOne,CompTwo">
+    <!-- <KeepAlive include="CompOne,CompTwo"> -->
+    <KeepAlive :max="1">
         <component :is="activeComp"></component>
     </KeepAlive>
 </template>
